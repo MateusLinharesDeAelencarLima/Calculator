e2532c6812a8168380bf9054fa8c1c38a1d008b9
Mateus Linhares <mateuslalencar@gmail.com>
2021-05-08 11:02:48 -0300

    Create a multiplication function

---
+++ calculations/multiplication.py
@@ -0,0 +1,7 @@
+def multiplication(num1, num2):
+    total = num1 * num2
+
+    if (total % 1) == 0:
+        return int(total)
+    else:
+        return total
--- calculator.py
+++ calculator.py
@@ -1,11 +1,14 @@
 from calculations.summation import summation
 from calculations.subtraction import subtraction
+from calculations.multiplication import multiplication
 
 num1 = float(input('numero 1: '))
 num2 = float(input('numero 2: '))
 
 operation_1 = summation(num1, num2)
 operation_2 = subtraction(num1, num2)
+operation_3 = multiplication(num1, num2)
 
 print("A soma dos números é:", operation_1)
 print("A subtração dos números é:", operation_2)
+print("O produto dos números é:", operation_3)
