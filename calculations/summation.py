87a5931bb7549888954ad4061a4e1fb7ec83581d
Mateus Linhares <mateuslalencar@gmail.com>
2021-05-08 09:35:07 -0300

    Create a summation function

---
+++ calculations/summation.py
@@ -0,0 +1,7 @@
+def summation(num1, num2):
+    total = num1 + num2
+
+    if (total % 1) == 0:
+        return int(total)
+    else:
+        return total
---
+++ calculator.py
@@ -0,0 +1,9 @@
+from calculations.summation import summation
+
+num1 = float(input('numero 1: '))
+num2 = float(input('numero 2: '))
+
+total = summation(num1, num2)
+
+
+print(total)
