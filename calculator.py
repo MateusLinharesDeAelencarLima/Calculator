8a91a762b5268c062758a71b0fec632432aa64a5
Mateus Linhares <mateuslalencar@gmail.com>
2021-05-08 10:38:23 -0300

    Bug fix

--- calculator.py
+++ calculator.py
@@ -7,5 +7,5 @@ num2 = float(input('numero 2: '))
 operation_1 = summation(num1, num2)
 operation_2 = subtraction(num1, num2)
 
-print("A soma dos números é: ", operation_1)
-print("A subtração dos números é: ", operation_2)
+print("A soma dos números é:", operation_1)
+print("A subtração dos números é:", operation_2)
