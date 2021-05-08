8f9cbc49b1f11df7d500a7be352db027b9905c29
Mateus Linhares <mateuslalencar@gmail.com>
2021-05-08 10:36:42 -0300

    Create a subtraction function

---
+++ calculations/subtraction.py
@@ -0,0 +1,7 @@
+def subtraction(num1, num2):
+    total = num1 - num2
+
+    if (total % 1) == 0:
+        return int(total)
+    else:
+        return total
--- calculator.py
+++ calculator.py
@@ -1,9 +1,11 @@
 from calculations.summation import summation
+from calculations.subtraction import subtraction
 
 num1 = float(input('numero 1: '))
 num2 = float(input('numero 2: '))
 
-total = summation(num1, num2)
+operation_1 = summation(num1, num2)
+operation_2 = subtraction(num1, num2)
 
-
-print(total)
+print("A soma dos números é: ", operation_1)
+print("A subtração dos números é: ", operation_2)
