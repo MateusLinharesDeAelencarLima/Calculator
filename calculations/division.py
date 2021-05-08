709ef6e77c63b7dd0bbadb25060024de82db54f8
Mateus Linhares <mateuslalencar@gmail.com>
2021-05-08 13:46:06 -0300

    Create a division function

---
+++ calculations/division.py
@@ -0,0 +1,7 @@
+def division(num1, num2):
+    total = num1 / num2
+
+    if (total % 1) == 0:
+        return int(total)
+    else:
+        return total
--- calculator.py
+++ calculator.py
@@ -1,6 +1,7 @@
 from calculations.summation import summation
 from calculations.subtraction import subtraction
 from calculations.multiplication import multiplication
+from calculations.division import division
 
 num1 = float(input('numero 1: '))
 num2 = float(input('numero 2: '))
@@ -8,7 +9,9 @@ num2 = float(input('numero 2: '))
 operation_1 = summation(num1, num2)
 operation_2 = subtraction(num1, num2)
 operation_3 = multiplication(num1, num2)
+operation_4 = division(num1, num2)
 
 print("A soma dos números é:", operation_1)
-print("A subtração dos números é:", operation_2)
+print("A diferença dos números é:", operation_2)
 print("O produto dos números é:", operation_3)
+print("O quociente dos números é:", operation_4)
